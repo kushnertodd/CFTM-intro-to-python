2d8ca6dbab2e939e0a645b4f7564f65d2a0fe548
Todd Kushner <kushnertodd@gmail.com>
2023-04-12 09:31:35 -0600

    update

--- runjupyter1.bat
+++ runjupyter1.bat
@@ -1,5 +1,5 @@
 cd C:\Users\kushn\Dropbox\software\projects\books\CFTM-intro-to-python
 rem 'jupyter-lab' after installing with 'pip install jupyterlab'
 rem python3 -m jupyterlab 
-python -m jupyterlab 
-rem jupyter-lab
+rem py -m jupyterlab 
+jupyter-lab
