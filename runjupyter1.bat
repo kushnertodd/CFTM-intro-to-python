8c0d997a3a6c2765e77aba3532ed494e697eb927
Todd Kushner <kushnertodd@gmail.com>
2024-01-19 10:03:43 -0700

    refactored directory structure

--- runjupyter1.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\kushn\Dropbox\software\projects\books\CFTM-intro-to-python
-rem 'jupyter-lab' after installing with 'pip install jupyterlab'
-rem python3 -m jupyterlab 
-rem py -m jupyterlab 
-jupyter-lab
