026b8a8a80e0d78628e019d2357fe47b7023e274
Todd Kushner <kushnertodd@gmail.com>
2024-01-22 09:48:40 -0700

    protect

--- bin/protect.sh
+++ bin/protect.sh
@@ -1,9 +1,8 @@
 cd /home/kushnertodd_toddkushnerllc_com/CFTM-intro-to-python
-chmod 555 . \
-	notebooks \
-	bin
+chmod 111 bin
+chmod 555 . notebooks 
 chmod 444 \
 	LICENSE \
 	README.md \
 	python_intro.ipynb \
-	notebooks/*
+	notebooks
